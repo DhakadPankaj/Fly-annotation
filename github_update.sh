7b117ab70db06145d576ad588fc9474f2097af29
Pankaj Dhakad <s2215768@vera.bio.ed.ac.uk>
2022-04-06 11:08:22 +0100

    06_April_2022

--- github_update.sh
+++ github_update.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 cd /data/home/s2215768/fly_annotation/scripts
+
+git pull origin main
 git add .
 git commit -m "$1"
 PASS="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"
 
 xyz=$(expect -c "
-spawn git push origin main 
+spawn git push origin main
 expect \"Username for 'https://github.com':\"
 send \"pd16\r\"
 expect \"Password for 'https://pd16@github.com':\"
